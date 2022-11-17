c152382ef836cae3177a94b0bba4e149216b446d
John Murray <johnmurray@fastmail.com>
2022-11-17 10:00:59 -0500

    add refs for tag

--- note-app/src/NoteForm.jsx
+++ note-app/src/NoteForm.jsx
@@ -1,22 +1,48 @@
 import { Form, Stack, Row, Col, Button } from "react-bootstrap";
 import CreateableReactSelect from "react-select/creatable";
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
 
-export function NoteForm() {
+export function NoteForm({ onSubmit }) {
+  const titleRef = useRef(null);
+  const markdownRef = useRef(null);
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({
+      title: titleRef.current.value,
+      markdown: markdownRef.current.value,
+      tags: [],
+    });
+  };
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Stack gap={4}>
         <Row>
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required></Form.Control>
+              <Form.Control ref={titleRef} required></Form.Control>
             </Form.Group>
           </Col>
           <Col>
             <Form.Group controlId="tags">
               <Form.Label>Tags</Form.Label>
-              <CreateableReactSelect isMulti />
+              <CreateableReactSelect
+                isMulti
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+              />
             </Form.Group>
           </Col>
         </Row>
@@ -24,7 +50,12 @@ export function NoteForm() {
           <Col>
             <Form.Group controlId="body">
               <Form.Label>Body</Form.Label>
-              <Form.Control required as="textarea" rows={15}></Form.Control>
+              <Form.Control
+                required
+                as="textarea"
+                ref={markdownRef}
+                rows={15}
+              ></Form.Control>
             </Form.Group>
           </Col>
         </Row>
